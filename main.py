9c9ab24992052a5ff440ec67795275d49a45fcc8
janvichr001 <janvichr001@warren.k12.in.us>
2024-08-28 09:57:07 -0400

    commit input

--- main.py
+++ main.py
@@ -2,9 +2,3 @@ name = input("What is your name: ")
 droids  = input("how many droids they want to meet ")
 wookiees = input( " how many wookiees they want to meet ")
 print(name, "wants to meet", droids, "droids and ", wookiees, "Wookiees!")
-#INSTRUCTION:  write an input
-# statement that asks for
-# number of droid and store as droids
-#INSTRUCTION:  write an input
-# statement that asks for
-# number of wookies and store as wookiees
